d818a1194c09aa31def40e2f82eb56d1f9a5a3ce
Kavishree24 <kavishreesathishkumar24@gmail.com>
2025-08-27 19:14:23 +0530

    Thirdparty spec added

--- thirdparty/src/main/java/com/raavana/thirdparty/ThirdPartyServiceApplication.java
+++ thirdparty/src/main/java/com/raavana/thirdparty/ThirdPartyServiceApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableDiscoveryClient
+
+
 public class ThirdPartyServiceApplication {
 
 	public static void main(String[] args) {
