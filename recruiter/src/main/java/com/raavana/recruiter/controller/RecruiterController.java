47cb0b4135ec0a1ac7a62971bdd96756accd9fea
Dhanushpandi123 <dhanushpandijeyapal@gmail.com>
2025-08-28 16:03:58 +0530

    recruiter spec added

--- recruiter/src/main/java/com/raavana/recruiter/controller/RecruiterController.java
+++ recruiter/src/main/java/com/raavana/recruiter/controller/RecruiterController.java
@@ -1,6 +1,8 @@
 package com.raavana.recruiter.controller;
 
+import com.raavana.recruiter.api.HelloApi;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/recruiter")
-public class RecruiterController {
+public class RecruiterController implements HelloApi {
 
         @GetMapping("/hello")
         public String sayHello() {
             return "Hello from Recruiter Service!";
         }
 
+    @Override
+    public ResponseEntity<String> helloGet() {
+        return ResponseEntity.ok("Hello from Recruiter Service!");
+    }
 }
