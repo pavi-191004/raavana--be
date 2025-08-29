24b2b6c2b984a5a1e0607033abcca12606767b5b
jeyasree123 <jeyasreechandran123@gmail.com>
2025-08-29 16:13:43 +0800

    swagger spec is added

--- admin/src/main/java/com/raavana/admin/controller/AdminController.java
+++ admin/src/main/java/com/raavana/admin/controller/AdminController.java
@@ -1,12 +1,20 @@
 package com.raavana.admin.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import com.raavana.admin.api.HiApi;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
-public class AdminController {
-    @GetMapping("/get")
-    public String getApi(){
-        return "Getting API";
-    }
+public class AdminController implements HiApi{
+
+
+
+        @Override
+        public ResponseEntity<String> hiGet() {
+            return ResponseEntity.ok("Hello from Admin Service!");
+        }
+
+
 }
