d527e8a0d2dc8cbe87288ae0aa6664ba0cddf189
jeyasree123 <jeyasreechandran123@gmail.com>
2025-09-06 19:05:00 +0800

    Api gateway is added

--- admin/src/main/java/com/raavana/admin/controller/AdminController.java
+++ admin/src/main/java/com/raavana/admin/controller/AdminController.java
@@ -2,11 +2,13 @@ package com.raavana.admin.controller;
 
 import org.springframework.http.ResponseEntity;
 import com.raavana.admin.api.HiApi;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 
 @RestController
+@RequestMapping("/api")
 public class AdminController implements HiApi{
 
 
