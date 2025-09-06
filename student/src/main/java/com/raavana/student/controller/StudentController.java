f1445f3f44a7374b8fcac4af973aa481163fda27
ShriShadhaJambudurai <shrishadhajambudurai@gmail.com>
2025-09-06 15:25:58 +0530

    api-gateway is added

--- student/src/main/java/com/raavana/student/controller/StudentController.java
+++ student/src/main/java/com/raavana/student/controller/StudentController.java
@@ -3,14 +3,16 @@ package com.raavana.student.controller;
 import com.raavana.student.api.TestApi;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class StudentController implements TestApi {
 
 
     @Override
     public ResponseEntity<String> testGet() {
-        return ResponseEntity.ok("Hello from student Service!");
+        return ResponseEntity.ok("Hello from Student Service!");
     }
 }
