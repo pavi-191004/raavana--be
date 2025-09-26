a0c4626b8bddeabc1e84c901fab990c6caacc449
sridhar-s-sri <sridharsubbaiya1234@gmail.com>
2025-09-26 16:18:15 +0530

    Admin Onboadring Created

---
+++ admin/src/main/java/com/raavana/admin/entity/PersonalEntity.java
@@ -0,0 +1,27 @@
+package com.raavana.admin.entity;
+
+import com.raavana.admin.model.PersonalInfo;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection="adminDetails")
+
+public class PersonalEntity {
+
+    @Id
+    private String id;
+    private String fullName;
+    private String designation;
+    private Long contactNumber;
+    private PersonalInfo.RoleIdEnum roleId;
+
+
+}
