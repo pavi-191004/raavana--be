97063e0d779b465150f3e3ea56d77b8381a4295b
dharini-sri10 <dharinisri1012005@gmail.com>
2025-10-08 19:29:40 +0530

    Compress Base64 by 60%

--- thirdparty/src/main/java/com/raavana/thirdparty/utils/PdfCompressionException.java
+++ thirdparty/src/main/java/com/raavana/thirdparty/utils/PdfCompressionException.java
@@ -1,10 +1,18 @@
 package com.raavana.thirdparty.utils;
 
 public class PdfCompressionException extends Exception {
-    public PdfCompressionException() { super(); }
-    public PdfCompressionException(String message) { super(message); }
-    public PdfCompressionException(String message, Throwable cause) { super(message, cause); }
-    public PdfCompressionException(Throwable cause) { super(cause); }
+    public PdfCompressionException() {
+        super();
+    }
+    public PdfCompressionException(String message) {
+        super(message);
+    }
+    public PdfCompressionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PdfCompressionException(Throwable cause) {
+        super(cause);
+    }
 }
 
 
