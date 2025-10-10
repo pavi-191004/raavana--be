1fdaff90693a9f4b04536f4cf0f5d515fd8f3f68
apple <ramprasanthsankaranarayanan@gmail.com>
2025-10-10 20:22:53 +0530

    changes updated

--- student/src/main/java/com/raavana/student/service/StudentOnboardingService.java
+++ student/src/main/java/com/raavana/student/service/StudentOnboardingService.java
@@ -4,21 +4,28 @@ import com.raavana.student.entity.StudentOnboardingEntity;
 import com.raavana.student.model.StudentDTO;
 import com.raavana.student.repository.StudentOnboardingRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j; // ✅ Add this import
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class StudentOnboardingService {
 
     private final StudentOnboardingRepository repository;
 
     public StudentDTO saveStudent(StudentDTO studentDTO) {
+        log.info("Saving student data for userId: {}", studentDTO.getUserId());
+
         StudentOnboardingEntity entity = StudentOnboardingEntity.builder()
                 .userId(studentDTO.getUserId())
                 .education(studentDTO.getEducation())
                 .build();
 
         repository.save(entity);
+
+
+        log.info("Student data saved successfully for userId: {}", studentDTO.getUserId());
 
         return studentDTO;
     }
